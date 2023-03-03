f5ec53195f8a156a9e480ed7bbc2445ef6f41bdc
khe <khj778878@naver.com>
2023-03-03 13:59:59 +0900

    [LV 1]: 자연수 뒤집어 배열로 만들기_solution 추가

--- LV 1/자연수 뒤집어 배열로 만들기.py
+++ LV 1/자연수 뒤집어 배열로 만들기.py
@@ -4,3 +4,8 @@ def solution(n):
     answer = list(map(int,list(numStr)))
     answer.reverse()
     return answer
+
+# 최적의 풀이 : 입력받은 int숫자형을 string으로 변환해서 reversed()함수 이용 (입력받은 숫자가 문자열 형태로 뒤집힌다.)
+#               -> map 함수를 통해 뒤집힌 문자열을 int형으로 변환 후 리스트로 만든다.
+def digit_reverse(n):
+    return list(map(int, reversed(str(n))))
