5d5a3234f34fa7d45099922ddacfe5ebf07e365c
khe <khj778878@naver.com>
2023-03-21 21:03:24 +0900

    [LV 2]: 이진 변환 반복하기

---
+++ LV 2/이진 변환 반복하기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    cnt, removeTotal = 0, 0
+    while s != "1":
+        total = len(s)
+        s = s.replace("0", "")
+        removeZero = len(s)
+        removeTotal += total - removeZero
+        s = bin(removeZero)[2:]
+        cnt += 1
+
+    return [cnt, removeTotal]
