92ca19997f1b7ffe3385ac5a2b3e8a6c245af962
khe <khj778878@naver.com>
2023-04-05 17:29:55 +0900

    [LV 2]: 디펜스 게임_idea참고, 최대힙 사용한 solution추가

---
+++ LV 2/디펜스 게임.py
@@ -0,0 +1,67 @@
+import heapq as hq
+def solution(n, k, enemy):
+    if len(enemy) == k:
+        return len(enemy)
+    
+    r = 0
+    soldier = [] # 통과한 라운드에 사용된 병사의 수를 담는 최대힙
+    for i in range(len(enemy)):
+        # 가지고 있는 병사를 사용하여 라운드 통과하는 경우
+        if n >= enemy[i]:
+            n -= enemy[i]
+            hq.heappush(soldier, -enemy[i]) 
+            r += 1
+            
+        # 병사의 수가 모자른 경우 
+        else:
+            # 남은 무적권의 갯수가 1개 이상인 경우에만 추가 라운드 진행 가능
+            if k > 0:
+                # 현재 라운드의 적의 수보다 최대힙의 top에 위치한 병사의 수가 더 많은 경우 -> 해당 라운드에 사용된 병사의 수 만큼 도로 가져와서 현재 병사의 수에 더한다. 
+                # 그리고 이미 통과한 라운드에 무적권을 사용하는 것으로 변경(롤백)
+                if soldier and -soldier[0] > enemy[i]:
+                    n += -hq.heappop(soldier)
+                    n -= enemy[i]
+                    hq.heappush(soldier, -enemy[i]) # 해당 라운드에 사용된 병사 수를 최대힙에 넣어줌
+                
+                # 최대힙이 비었거나 현재 라운드의 적의 수가 최대힙의 top에 위치한 병사의 수 이하라면, 그냥 현재 라운드에 무적권을 사용
+                else: 
+                    pass
+                
+                k -= 1
+                r += 1
+                
+            else:
+                break
+                
+    return r       
+
+
+
+'''
+* 참고한 아이디어 -> 최대힙 이용 : 지나온 라운드와 현재 라운드 중에서 가장 병사의 수가 많은 라운드에 무적권을 적용하는 방식
+1. 우선 적의 수를 순서대로 우선순위 큐에 저장한다. 
+2. 그리고 만약 병사의 수(n)가 0보다 적으면 무적권을 사용한다.
+3. 이 때, 무적권은 우선순위 큐에 저장된 적의 수 중에서 가장 큰 값에 대해서 사용한다. 
+-> 즉, 이미 병사의 수를 차감하여 라운드를 통과하였지만 다시 롤백하여 우선순위 큐의 가장 top에 위치한 값(사용한 병사의 수)만큼 다시 가져오고 
+  해당 라운드(이미 지나온)에는 무적권을 사용하는 방식. 
+  이 때 추가적으로 생각해야 할 부분은, 만약 현재 라운드의 적의 수(enemy[i])보다 최대힙의 top에 위치한 병사의 수가 더 적다면 그냥 현재 라운드에 무적권을 적용하고 넘어간다.
+'''
+
+''' 첫번째 코드 -> 40.6점... 이런식으로 경우의수 파악하는건 불가능하다고 판단 -> 풀이 참고하여 힙큐를 사용하는 코드로 수정
+def solution(n, k, enemy):
+    if len(enemy) == k:
+        return len(enemy)
+    
+    r, m = 0, n
+    for i in range(len(enemy)):
+        if m >= enemy[i]:
+            m -= enemy[i]
+            r += 1
+        else:
+            if k > 0:
+                k -= 1
+                r += 1  
+            else: 
+                break
+    return r            
+'''
