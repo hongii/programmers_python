101af32e8f743f56ffdbda852cf0d4bcdd9624a1
khe <khj778878@naver.com>
2023-04-28 17:19:07 +0900

    [LV 2]: 큰 수 만들기_그리디(stack이용)

---
+++ LV 2/큰 수 만들기.py
@@ -0,0 +1,27 @@
+# 그리디(탐욕법) -> stack이용 (다른사람 풀이 참고)
+def solution(number, k):
+    stack = []
+    for n in number:
+        # stack의 top에 위치한 값이 현재 숫자(n)보다 작은 경우, n보다 크거나 같은 수가 나올때까지 pop()수행
+        while stack and stack[-1] < n and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(n)
+
+    # 아직 제거 해야할 횟수가 남은경우, 일의자리 숫자부터 제거
+    if k > 0:
+        while k > 0 :
+            k -= 1
+            stack.pop()
+
+    return "".join(stack)
+
+
+''' 시간 초과 발생 -> number의 자릿수가 100만 이므로 combinations() 사용 시 시간초과 발생
+from itertools import combinations
+def solution(number, k):
+    size = len(number) - k
+    cb = list(combinations(list(number), size))
+    cb.sort(reverse=True)
+    return "".join(cb[0])
+'''
