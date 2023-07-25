e26e559c6df0e4c67981dcae489847ceb9ea6e87
khe <khj778878@naver.com>
2023-07-25 12:53:09 +0900

    [LV 2]: 메뉴 리뉴얼_풀이 추가(2회차)

--- LV 2/메뉴 리뉴얼.py
+++ LV 2/메뉴 리뉴얼.py
@@ -1,3 +1,25 @@
+# 두번째 풀이 -> 2회차
+from itertools import combinations
+from collections import Counter
+def solution(orders, course):
+    res = []
+    for x in course:
+        counter = Counter()
+        for order in orders:
+            cb = Counter(combinations(sorted(order), x))
+            counter += cb
+        if not counter: continue
+            
+        sortedCounter = counter.most_common()
+        maxCnt = sortedCounter[0][1]
+        for tup, cnt in sortedCounter:
+            if cnt == maxCnt and maxCnt > 1:
+                res.append("".join(list(tup)))
+            else: 
+                break
+    return sorted(res)
+
+''' 첫번째 코드
 from itertools import combinations
 def solution(orders, course):
     res = []
@@ -26,3 +48,4 @@ def solution(orders, course):
 
     res.sort()
     return res
+'''
