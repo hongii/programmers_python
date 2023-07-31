17da735adfbf6e0693acce04dd97e3408652d2e0
khe <khj778878@naver.com>
2023-07-31 13:02:17 +0900

    [LV 2]: 구명보트_2회차

--- LV 2/구명보트.py
+++ LV 2/구명보트.py
@@ -1,3 +1,20 @@
+# 2회차 코드
+def solution(people, limit):
+    people.sort(reverse=True)
+    l, r = 0, len(people)-1
+    cnt = 0
+    while l <= r:
+        if people[l]+people[r] > limit:
+            l += 1
+        else:
+            l += 1
+            r -= 1
+        cnt += 1
+    return cnt
+
+
+
+# 1회차 코드
 # python_algorithm : [그리디 알고리즘] 침몰하는 타이타닉 문제와 유사 -> 다시 풀기
 from collections import deque
 def solution(people, limit):
