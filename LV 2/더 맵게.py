bf734ed35ff3b17cdc6b3adfd6af3e34f16bfab3
khe <khj778878@naver.com>
2023-08-04 17:25:14 +0900

    [LV 2]: 더 맵게_2회차

--- LV 2/더 맵게.py
+++ LV 2/더 맵게.py
@@ -1,3 +1,17 @@
+# 2회차 코드
+import heapq as hq
+def solution(scoville, K):
+    hq.heapify(scoville)
+    cnt = 0
+    while len(scoville) > 1 and scoville[0] < K:
+        min1 = hq.heappop(scoville)
+        min2 = hq.heappop(scoville)
+        hq.heappush(scoville, min1 + min2*2)
+        cnt += 1
+    return cnt if scoville[0] >= K else -1
+
+
+# 1회차 코드
 import heapq
 def solution(scoville, K):
     heapq.heapify(scoville)
