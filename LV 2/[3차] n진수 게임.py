6e2a9ebe638f7a32ddc9bb6fb95ea368eff4de2e
khe <khj778878@naver.com>
2023-08-10 13:21:23 +0900

    [LV 2]: [3차] n진수 게임_2회차

--- LV 2/[3차] n진수 게임.py
+++ LV 2/[3차] n진수 게임.py
@@ -1,3 +1,30 @@
+# 2회차 코드
+def convert(n, num):
+    global numList
+    convertNum = ""
+    dic= {"10":"A", "11":"B", "12":"C", "13":"D", "14":"E", "15":"F"}
+    while num > 0:
+        num, mod = divmod(num, n)
+        if mod >= 10:
+            convertNum += dic[str(mod)]
+        else:
+            convertNum += str(mod)   
+    numList.extend(list(convertNum[::-1]))
+    
+def solution(n, t, m, p):
+    global numList
+    i = p-1
+    numList, res = ["0"], []
+    for num in range(t*m):
+        convert(n, num)
+        while i < len(numList):
+            res.append(numList[i])
+            i += m
+            if len(res) == t:
+                return "".join(res)
+
+
+# 1회차 코드
 def convert(n, i):
     res = []
     dic = {10:"A", 11:"B", 12:"C", 13:"D", 14:"E", 15:"F"}
