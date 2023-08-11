dd35dc16aa2d37f1cb01c59fbc05350038970f7d
khe <khj778878@naver.com>
2023-08-11 12:43:25 +0900

    [LV 2]: [3차] 압축_2회차

--- LV 2/[3차] 압축.py
+++ LV 2/[3차] 압축.py
@@ -1,3 +1,21 @@
+# 2회차 코드 -> 시간 절약
+def solution(msg):
+    dic = {chr(key):key-ord("A")+1 for key in range(ord("A"), ord("Z")+1)}
+    res = []
+    nextVal = 27
+    while msg:
+        i = 0
+        while i < len(msg) and msg[:i+1] in dic.keys():
+            i += 1
+        if msg[:i+1] not in dic.keys():
+            dic[msg[:i+1]] = nextVal
+        res.append(dic[msg[:i]])
+        nextVal += 1
+        msg = msg[i:]
+    return res
+
+
+# 1회차 코드 -> 시간 오래 걸림
 import re
 def solution(msg):
     dic = {chr(ord("A")+i-1):i for i in range(1, 27)}
