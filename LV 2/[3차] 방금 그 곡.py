af66cbe6f142c2cd8e8313e21ad3c00fb5e92812
khe <khj778878@naver.com>
2023-08-27 13:57:26 +0900

    [LV 2]: [3차]방금 그 곡

--- LV 2/[3차] 방금 그 곡.py
+++ LV 2/[3차] 방금 그 곡.py
@@ -1,3 +1,26 @@
+# 2회차 코드
+import re
+def solution(m, musicinfos):
+    res = []
+    m = m.replace("C#","c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#","a")
+    for i, info in enumerate(musicinfos):
+        infoList = info.split(",")
+        start_h, start_m = int(infoList[0][:2]), int(infoList[0][3:])
+        end_h, end_m = int(infoList[1][:2]), int(infoList[1][3:])
+        playTime = (end_h*60 + end_m) - (start_h*60 + start_m)
+        title, codes = infoList[2], infoList[3]
+        codes = codes.replace("C#","c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#","a")
+        if playTime > len(codes):
+            repeat = playTime//len(codes) + 1
+            codes = codes * repeat
+            
+        if re.search(m, codes[:playTime]):
+            res.append((playTime, i, title))
+    
+    return sorted(res, key=lambda x:(-x[0], x[1]))[0][2] if len(res) > 0 else "(None)"
+
+
+# 1회차 코드
 import math
 import re
 def solution(m, musicinfos):
