0b410239e89f97965612a2919eb2f897c30f4b0c
khe <khj778878@naver.com>
2025-04-22 14:38:46 +0900

    [LV 2]: 연속된 부분 수열의 합_투포인터

--- LV 2/연속된 부분 수열의 합.py
+++ LV 2/연속된 부분 수열의 합.py
@@ -1,3 +1,21 @@
+# 2회차(투포인터)
+def solution(sequence, k):
+    l = len(sequence)
+    res = [0, l-1]
+    end = 0
+    seq_sum = 0
+    for start in range(l):
+        while end < l and seq_sum < k:
+            seq_sum += sequence[end]
+            end += 1
+
+        if seq_sum == k and res[1] - res[0] + 1 > end - start:
+            res = [start, end-1]
+        
+        seq_sum -= sequence[start] 
+
+    return res
+
 # 투포인터
 def solution(sequence, k):
     seq_len = len(sequence)
